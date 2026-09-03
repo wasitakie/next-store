3c3bd83568e7174f16f5655474ac139d97b8ddf1
wasitakie <wasitaya@gmail.com>
2026-09-03 20:42:49 +0700

    Edit config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   images: {
     unoptimized: true,
     remotePatterns: [
